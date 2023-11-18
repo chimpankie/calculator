377743c09ee570c3bdd9332ade7c83852cee4747
Kieron Rees <140263951+chimpankie@users.noreply.github.com>
2023-11-18 20:20:32 +0000

    Make number presses appear on display

--- script.js
+++ script.js
@@ -3,7 +3,26 @@
 
 let a = 0;
 let b = 0;
-let operator = ""
+let operator = "";
+
+// Display variable
+
+let displayContent = '';
+let display = document.querySelector('#displayText');
+let numberBtns = document.querySelectorAll('.number');
+
+
+
+// Update display if someone presses a number, hold total in displayContent
+
+numberBtns.forEach(function (numberBtn){
+    numberBtn.addEventListener('click', () => {
+        displayContent += numberBtn.textContent;
+        display.textContent = displayContent;
+    })
+})
+
+
 
 // operator function
 
