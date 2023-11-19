1d22a2b6253f2f2603f2996e7a95fe6a01e38cb8
Kieron Rees <140263951+chimpankie@users.noreply.github.com>
2023-11-19 19:04:42 +0000

    Create a function to handle operations

--- script.js
+++ script.js
@@ -3,15 +3,41 @@
 
 let a = 0;
 let b = 0;
-let operator = "";
+let operator = 0;
+let runningTotal = 0;
 
-// Display variable
+
+// Display variable and button query selectors
 
 let displayContent = '';
 let display = document.querySelector('#displayText');
 let numberBtns = document.querySelectorAll('.number');
+let operatorBtns = document.querySelectorAll('.operator');
 
 
+// Act when operator button pressed
+
+// something is very broken here... 
+operatorBtns.forEach(function (operatorBtn){
+    operatorBtn.addEventListener('click', () => {
+    if (operator){
+        b = displayContent;
+        a = Number(a);
+        b = Number(b);
+        a = operate(a, b, operator);
+        console.log(a);
+        displayContent = '';
+        display.textContent = a;
+        operator = operatorBtn.textContent; 
+    } else {
+        a = displayContent;
+        runningTotal = a;
+        operator = operatorBtn.textContent;
+        displayContent = '';
+        display.textContent = 0; 
+    }
+})
+})
 
 // Update display if someone presses a number, hold total in displayContent
 
